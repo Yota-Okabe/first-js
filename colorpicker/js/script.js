9c0dc045f0719ed59e0fc06fcfc76450e414795b
Yota Okabe <93124377+Yota-Okabe@users.noreply.github.com>
2023-06-19 15:42:31 +0900

    通常値 + 取得値

--- colorpicker/js/script.js
+++ colorpicker/js/script.js
@@ -1,2 +1,2 @@
 console.log(document.querySelector('#colorPicker') . value);
-document.querySelector('#colorText') . textContent = 'カラーコード：';
+document.querySelector('#colorText') . textContent = `カラーコード：${document.querySelector('#colorPicker').value}`;
