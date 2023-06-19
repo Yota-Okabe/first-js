6d2885d40cb54cf3d47ca40ee2873d955a46f0eb
Yota Okabe <93124377+Yota-Okabe@users.noreply.github.com>
2023-06-19 16:24:50 +0900

    カラーコードと背景色を連動

--- colorpicker/js/script.js
+++ colorpicker/js/script.js
@@ -4,7 +4,10 @@ const text = document.querySelector('#colorText');
 const color = document.querySelector('#colorPicker');
 
 const colorBg = () => {
+    // 選択したカラーコードを表示
     text.textContent = `カラーコード：${color.value}`;
+    // 選択したカラーを背景色に反映
+    document.body.style.backgroundColor = color.value;
 }
 
 color.addEventListener('input', colorBg);
